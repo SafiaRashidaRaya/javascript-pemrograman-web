f1084a80517f2065b9a89780ca4827a607f388c8
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2023-10-01 21:45:30 +0700

    versi 1.4
    querySelectorAll Javascript

--- javascript.js
+++ javascript.js
@@ -1,5 +1,14 @@
 function tampil(){
-    document.querySelector("h1").innerText = "Belajar Javascript";
-    document.querySelector(".container").style.backgroundColor = "blue";
-    document.querySelector("#para").style.fontSize = "100px"
+    // document.querySelector("h1").innerText = "Belajar Javascript";
+    // document.querySelector(".container").style.backgroundColor = "blue";
+    // document.querySelector("#para").style.fontSize = "100px"
+
+    // console.log(document.querySelectorAll("img"));
+
+    min = Math.ceil(0);
+    max = Math.floor(2);
+    let m = Math.floor(Math.random() * (max - min + 1) + min);
+    console.log(m);
+
+    document.querySelectorAll("img")[m].style.visibility="visible";
 }
